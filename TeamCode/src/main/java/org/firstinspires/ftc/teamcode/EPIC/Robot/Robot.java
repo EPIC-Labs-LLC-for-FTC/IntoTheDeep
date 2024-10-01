f412ce332fadd05c7bdedd68def202d3b849a0e7
oobty <zaidnajmi539@gmail.com>
2024-10-01 06:24:46 -0500

    Renamed Odyssey_Bot class to Robot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/Robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/Robot/Robot.java
@@ -1,19 +1,18 @@
 package org.firstinspires.ftc.teamcode.EPIC.Robot;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.EPIC.Components.Claw;
 import org.firstinspires.ftc.teamcode.EPIC.Components.Slider;
 import org.firstinspires.ftc.teamcode.EPIC.Components.Arm;
 import org.firstinspires.ftc.teamcode.EPIC.Components.Wrist;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Odyssey_Bot {
+public class Robot {
     public Claw odysseyClaw;
     public Slider odysseySlider;
     public Arm odysseyArm;
     public Wrist odysseyWrist;
 
-    public Odyssey_Bot(HardwareMap hardwareMap){
+    public Robot(HardwareMap hardwareMap){
         odysseyClaw = new Claw(hardwareMap);
         odysseySlider = new Slider(hardwareMap);
         odysseyArm = new Arm(hardwareMap);
