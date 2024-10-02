dbb391c91e981ab57cd1f4882903589eca035282
oobty <zaidnajmi539@gmail.com>
2024-10-02 10:51:25 -0500

    Update Robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/Robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/Robot/Robot.java
@@ -4,6 +4,8 @@ import org.firstinspires.ftc.teamcode.EPIC.Components.Claw;
 import org.firstinspires.ftc.teamcode.EPIC.Components.Slider;
 import org.firstinspires.ftc.teamcode.EPIC.Components.Arm;
 import org.firstinspires.ftc.teamcode.EPIC.Components.Wrist;
+import org.firstinspires.ftc.teamcode.EPIC.Motion.Mecanum_Wheels;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Robot {
@@ -11,11 +13,13 @@ public class Robot {
     public Slider odysseySlider;
     public Arm odysseyArm;
     public Wrist odysseyWrist;
+    public Mecanum_Wheels odysseyWheels;
 
     public Robot(HardwareMap hardwareMap){
         odysseyClaw = new Claw(hardwareMap);
         odysseySlider = new Slider(hardwareMap);
         odysseyArm = new Arm(hardwareMap);
         odysseyWrist = new Wrist(hardwareMap);
+        odysseyWheels = new Mecanum_Wheels(hardwareMap);
     }
 }
