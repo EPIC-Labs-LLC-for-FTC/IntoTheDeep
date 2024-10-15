3f7a4b5c040493314a9959595ebf618eaaf5e52f
oobty <zaidnajmi539@gmail.com>
2024-10-15 18:18:44 -0500

    Uncommented Robot Class code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/Robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/Robot/Robot.java
@@ -34,19 +34,19 @@ public class Robot implements IColorListener, ITouchListener, IClawListener {
 
     public Robot(LinearOpMode parent, String alliance){
         odysseyClaw = new Claw(parent.hardwareMap);
-//        odysseySlider = new Slider(parent.hardwareMap);
-//        odysseyArm = new Arm(parent.hardwareMap);
-//        odysseyWrist = new Wrist(parent.hardwareMap);
+        odysseySlider = new Slider(parent.hardwareMap);
+        odysseyArm = new Arm(parent.hardwareMap);
+        odysseyWrist = new Wrist(parent.hardwareMap);
         odysseyWheels = new Mecanum_Wheels(parent.hardwareMap);
         odysseyClaw.setParent(parent);
-//        odysseySlider.setParent(parent);
-//        odysseyArm.setParent(parent);
-//        odysseyWrist.setParent(parent);
+        odysseySlider.setParent(parent);
+        odysseyArm.setParent(parent);
+        odysseyWrist.setParent(parent);
         odysseyWheels.setParent(parent);
         odysseyClaw.setTelemetry(parent.telemetry);
-//        odysseySlider.setTelemetry(parent.telemetry);
-//        odysseyArm.setTelemetry(parent.telemetry);
-//        odysseyWrist.setTelemetry(parent.telemetry);
+        odysseySlider.setTelemetry(parent.telemetry);
+        odysseyArm.setTelemetry(parent.telemetry);
+        odysseyWrist.setTelemetry(parent.telemetry);
         odysseyWheels.setTelemetry(parent.telemetry);
         this.parent = parent;
         this.telemetry = parent.telemetry;
@@ -64,17 +64,17 @@ public class Robot implements IColorListener, ITouchListener, IClawListener {
     public void setIsAutonomous(boolean isAutonomous){
         this.isAutonomous = isAutonomous;
         odysseyClaw.setIsAutonomous(isAutonomous);
-//        odysseySlider.setIsAutonomous(isAutonomous);
-//        odysseyArm.setIsAutonomous(isAutonomous);
-//        odysseyWrist.setIsAutonomous(isAutonomous);
+        odysseySlider.setIsAutonomous(isAutonomous);
+        odysseyArm.setIsAutonomous(isAutonomous);
+        odysseyWrist.setIsAutonomous(isAutonomous);
         odysseyWheels.setIsAutonomous(isAutonomous);
     }
 
     public void initialize() {
         odysseyClaw.initialize();
-//        odysseySlider.initialize();
-//        odysseyArm.initialize();
-//        odysseyWrist.initialize();
+        odysseySlider.initialize();
+        odysseyArm.initialize();
+        odysseyWrist.initialize();
         odysseyWheels.initialize();
     }
 
