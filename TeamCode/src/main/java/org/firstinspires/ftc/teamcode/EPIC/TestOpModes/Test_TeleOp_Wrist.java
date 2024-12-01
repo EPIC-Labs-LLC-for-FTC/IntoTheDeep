9df30bc609d56cc336a69382c4d58178996b5ea3
oobty <zaidnajmi539@gmail.com>
2024-11-30 18:13:59 -0600

    Update Test_TeleOp_Wrist.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/TestOpModes/Test_TeleOp_Wrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/TestOpModes/Test_TeleOp_Wrist.java
@@ -5,10 +5,26 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.EPIC.Components.Wrist;
 
-@TeleOp(name = "Wrist Tester")
+@TeleOp(name = "Wrist_Tester")
 public class Test_TeleOp_Wrist extends LinearOpMode {
     public static double targetPosR = 0;
     public static double targetPosL = 0;
+
+    Thread wristing = new Thread() {
+        public void run() {
+            while (opModeIsActive()) {
+                if (gamepad1.a) {
+                    targetPosR += 0.1;
+                } else if (gamepad1.b) {
+                    targetPosR -= 0.1;
+                } else if (gamepad1.x) {
+                    targetPosL += 0.1;
+                } else if (gamepad1.y) {
+                    targetPosL -= 0.1;
+                }
+            }
+        }
+    };
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -18,9 +34,18 @@ public class Test_TeleOp_Wrist extends LinearOpMode {
 
         waitForStart();
 
+        wristing.start();
+
+        wrist.setPos(0);
+
         while (opModeIsActive()) {
-            wrist.jointR.setPosition(targetPosR);
-            wrist.jointL.setPosition(targetPosL);
+            if (gamepad1.right_bumper) {
+                wrist.jointR.setPosition(targetPosR);
+                wrist.jointL.setPosition(targetPosL);
+            }
+            telemetry.addData("JointR", targetPosR);
+            telemetry.addData("JointL", targetPosL);
+            telemetry.update();
         }
     }
 }
