e484fe5dbe7d77bcbef34f093113a2ee2b564003
oobty <zaidnajmi539@gmail.com>
2024-12-05 19:44:30 -0600

    Create Test_TeleOp_WristAndArm.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/TestOpModes/Test_TeleOp_WristAndArm.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.EPIC.TestOpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Test_TeleOp_WristAndArm extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
