4625b1f49f2762b060da3da1efc04cae84951ade
Nusaybah2011 <nusaybahniaziz@gmail.com>
2024-12-14 17:25:16 -0600

    Create Auton_SpecimenHang.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/OfficalOpModes/Auton_SpecimenHang.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.EPIC.OfficalOpModes;
+
+import org.firstinspires.ftc.teamcode.EPIC.RobotStates.ArmStates;
+
+public class Auton_SpecimenHang {
+
+     //(gamepad2.dpad_left) {
+         //open claw
+    //turn robot
+    //postion robot
+    //close claw
+    //(gamepad2.dpad_right) {
+        //odyssey.odysseyArm.move(ArmStates.SPECIMEN_DROP);
+    //wrist down
+    //(gamepad2.dpad_left)
+    //open claw
+    //wrist up
+    //y loop
+
+    }
+
+}
