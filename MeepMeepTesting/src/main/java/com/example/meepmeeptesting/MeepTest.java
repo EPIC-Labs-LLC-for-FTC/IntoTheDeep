92583fb80db189b7f47c989b251edcb7a8898e13
ayaz <111201806+Ayazislam6482@users.noreply.github.com>
2024-12-15 18:29:03 -0600

    EXP

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepTest.java
@@ -0,0 +1,69 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.DriveTrainType;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MeepTest {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(80, 80, Math.toRadians(180), Math.toRadians(180), 15.75)
+                .setDriveTrainType(DriveTrainType.MECANUM)
+                .setDimensions(15,18)
+                //RIGHT SIDE AUTO
+//                .setStartPose(new Pose2d(-14.3, -63, Math.toRadians(90)))
+                //LEFT SIDE AUTO
+                .setStartPose(new Pose2d(14.3, -63, Math.toRadians(90)))
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(myBot.getPose())
+
+                //LEFT SIDE AUTO
+
+                .setTangent(90)
+                .splineToConstantHeading(new Vector2d(9,-38), Math.PI/2)
+
+                .strafeToLinearHeading(new Vector2d(50,-40),-Math.PI/2)
+                .strafeToLinearHeading(new Vector2d(9,-38),Math.PI/2)
+
+                .strafeToConstantHeading(new Vector2d(36,-40))
+                .strafeToConstantHeading(new Vector2d(36,-10))
+
+                .strafeToConstantHeading(new Vector2d(45,-10))
+
+                //RIGHT SIDE AUTO
+
+//                .setTangent(90)
+//                .splineToConstantHeading(new Vector2d(-9,-38), Math.PI/2)
+//                .strafeToConstantHeading(new Vector2d(-28,-53))
+//                .strafeToLinearHeading(new Vector2d(-29,-32),Math.PI)
+//                .strafeToLinearHeading(new Vector2d(-40,-45),Math.PI*1.25)
+//
+//                .strafeToLinearHeading(new Vector2d(-45,-32),Math.PI)
+//                .strafeToLinearHeading(new Vector2d(-40,-45),Math.PI*1.25)
+//
+//                .strafeToLinearHeading(new Vector2d(-55,-32),Math.PI)
+//                .strafeToLinearHeading(new Vector2d(-40,-45),Math.PI*1.25)
+//
+//                .strafeToLinearHeading(new Vector2d(-55,-32),Math.PI)
+//                .splineToLinearHeading(new Pose2d(-25,-5,0), Math.PI/2)
+
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EXP_SPARK_AUTO/EXP_RIGHT_AUTO_SPARK.java
@@ -0,0 +1,165 @@
+package org.firstinspires.ftc.teamcode.EXP_SPARK_AUTO;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.RRsetup.SparkFunOTOSDrive;
+import org.firstinspires.ftc.teamcode.components.Arm;
+import org.firstinspires.ftc.teamcode.components.Claw;
+import org.firstinspires.ftc.teamcode.components.Slides;
+
+@Config
+@Autonomous(name = "EXP_RIGHT_AUTO_SPARK")
+public class EXP_RIGHT_AUTO_SPARK extends LinearOpMode {
+
+    public void close(){
+        Claw claw = new Claw(hardwareMap);
+        claw.close();
+    }
+
+    public void open(){
+        Claw claw = new Claw(hardwareMap);
+        claw.open();
+    }
+
+    public void barh(){
+        Slides slides = new Slides(hardwareMap);
+        slides.hBar();
+    }
+
+    public void barhReset(){
+        Slides slides = new Slides(hardwareMap);
+        slides.RESEThBar();
+    }
+
+    public void resetslides(){
+        Slides slides = new Slides(hardwareMap);
+        slides.resetslides();
+    }
+
+    public void anglearm(){
+        Arm arm = new Arm(hardwareMap);
+        arm.angle();
+    }
+
+    public void armdown(){
+        Arm arm = new Arm(hardwareMap);
+        arm.Horizontal();
+    }
+
+    public void slide0(){
+        Slides slides = new Slides(hardwareMap);
+        slides.start();
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Slides slides = new Slides(hardwareMap);
+        slides.setParent(this);
+        slides.setTelemetry(this.telemetry);
+        slides.initialize();
+
+        Arm arm = new Arm(hardwareMap);
+        arm.setParent(this);
+        arm.setTelemetry(this.telemetry);
+        arm.initialize();
+
+        Claw claw = new Claw(hardwareMap);
+        claw.setParent(this);
+        claw.setTelemetry(this.telemetry);
+        claw.initialize();
+
+        while (opModeInInit()){
+
+            arm.initialize();
+            claw.initialize();
+            slides.initialize();
+
+        }
+
+        Pose2d startPose = new Pose2d(14.3, -63, Math.toRadians(90));
+        SparkFunOTOSDrive sdrive = new SparkFunOTOSDrive(hardwareMap, startPose);
+
+        waitForStart();
+
+        Actions.runBlocking(
+
+                sdrive.actionBuilder(startPose)
+
+                        //PART1
+
+                        .afterTime(0.1,this::barh)
+                        .stopAndAdd(this::barh)
+
+                        .afterTime(0.1,this::anglearm)
+                        .stopAndAdd(this::anglearm)
+
+                        .setTangent(90)
+                        .splineToConstantHeading(new Vector2d(9,-38), Math.PI/2)
+
+                        .afterTime(0.1,this::barhReset)
+                        .stopAndAdd(this::barhReset)
+
+                        .waitSeconds(0.5)
+
+                        .afterTime(0.1,this::open)
+                        .stopAndAdd(this::open)
+
+                        //PART2
+
+                        .strafeToLinearHeading(new Vector2d(50,-40),-Math.PI/2)
+
+                        .afterTime(0.1,this::armdown)
+                        .stopAndAdd(this::armdown)
+
+                        .afterTime(0.1,this::slide0)
+                        .stopAndAdd(this::slide0)
+
+                        .afterTime(0.1,this::resetslides)
+                        .stopAndAdd(this::resetslides)
+
+                        .waitSeconds(1)
+
+                        .afterTime(0.1,this::close)
+                        .stopAndAdd(this::close)
+
+                        .afterTime(0.1,this::anglearm)
+                        .stopAndAdd(this::anglearm)
+
+                        .afterTime(0.1,this::barh)
+                        .stopAndAdd(this::barh)
+
+                        .strafeToLinearHeading(new Vector2d(9,-38),Math.PI/2)
+
+                        .afterTime(0.1,this::barhReset)
+                        .stopAndAdd(this::barhReset)
+
+                        .waitSeconds(0.5)
+
+                        .afterTime(0.1,this::open)
+                        .stopAndAdd(this::open)
+
+                        //PART3
+
+                        .strafeToConstantHeading(new Vector2d(36,-40))
+                        .strafeToConstantHeading(new Vector2d(36,-10))
+
+
+
+
+                        .build());
+
+        telemetry.addData("Slide1Position", slides.slide1.getCurrentPosition());
+        telemetry.addData("Slide2Position", slides.slide2.getCurrentPosition());
+        telemetry.addData("Arm1Position", arm.arm1.getPosition());
+        telemetry.addData("Arm2Position", arm.arm2.getPosition());
+        telemetry.addData("Claw1", claw.claw1.getPosition());
+        telemetry.addData("Claw2", claw.claw2.getPosition());
+        telemetry.update();
+    }
+}
