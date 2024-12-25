b06dec41f2f1653c9dc7ffbb0a5e183f463b8ba7
Nusaybah2011 <nusaybahniaziz@gmail.com>
2024-12-25 17:16:15 -0600

    Adding steps for coordinates
    - Added steps for Auton specimen hang with sparkfun

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -19,74 +19,28 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(80, 80, Math.toRadians(180), Math.toRadians(180), 15)
                 .setDriveTrainType(DriveTrainType.MECANUM)
-                .setDimensions(18,18)
-                .setStartPose(new Pose2d(9, -63, Math.toRadians(90)))
+                .setDimensions(16.5, 16.3)// length
+                .setStartPose(new Pose2d(8.25, -63.85, Math.toRadians(90)))
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(myBot.getPose())
-//                .lineToX(30)
-//                .turn(Math.toRadians(90))
-                .lineToY(-37.4)
-                        //.stopAndAdd() //specimen delivery action here
-//                        //.turn(Math.toRadians(-90))
-//                        //.lineToX(10)
-                        .strafeTo(new Vector2d(10,-37.4))
-                        //.turn(Math.toRadians(-60))
-                .splineToConstantHeading(new Vector2d(42,-9),Math.toRadians(90))
 
-                                //.splineToSplineHeading(new Pose2d(42,-9,Math.toRadians(90)),Math.toRadians(90))
-                //.splineToSplineHeading(new Pose2d(43,-9,Math.toRadians(60)),Math.toRadians(90))
-//                .strafeTo(new Vector2d(42,-9))
-//                                //.splineTo(new Vector2d(41,-9),Math.toRadians(-90))
-//                .turn(Math.toRadians(1))
-                                .lineToY(-52)
-//                        .lineToY(-20)
-//                .strafeTo(new Vector2d(52,-9))
-//                .turn(Math.toRadians(1))
+                .lineToY(-32.15)
+                .strafeTo(new Vector2d(10, -37.4))
+
+                .splineToConstantHeading(new Vector2d(42, -9), Math.toRadians(90))
+//
+//
 //                .lineToY(-52)
-//                .lineToY(-20)
-//                .strafeTo(new Vector2d(61,-9))
-//                .turn(Math.toRadians(-2))
-////
-                .splineTo(new Vector2d(51,-9),Math.toRadians(90))
-////                        //.strafeTo(new Vector2d(55,-9))
-//////                        .lineToX(55)
-//////                //.splineTo(new Vector2d(55,-9),Math.toRadians(0))
-                  .lineToY(-52)
-////
-////                .lineToY(-25)
-//                .splineTo(new Vector2d(40,-48),Math.toRadians(-270))
-//                                .stopAndAdd(new Action() {
-//                                    @Override
-//                                    public boolean run(@NotNull TelemetryPacket telemetryPacket) {
-//                                        try {
-//                                            Thread.sleep(2000);
-//                                        } catch (InterruptedException e) {
-//                                            throw new RuntimeException(e);
-//                                        }
-//                                        return false;
-//                                    }
-//                                })
-//                //action
-//                .splineTo(new Vector2d(8,-37.4),Math.toRadians(-180))
-//                //action
-//                .splineTo(new Vector2d(40,-48),Math.toRadians(-180))
-//                //action
-//                .splineTo(new Vector2d(8,-37.4),Math.toRadians(180))
-//                //action
-//                .splineTo(new Vector2d(40,-48),Math.toRadians(180))
-//                //action
-//                .splineTo(new Vector2d(8,-37.4),Math.toRadians(180))
-                .splineTo(new Vector2d(61,-9),Math.toRadians(90))
-//////                .lineToY(-9)
-//////                .lineToX(65)
-//////
-////////                .splineTo(new Vector2d(65,-9),Math.toRadians(0))
-                .lineToY(-52)
-//////                .lineToX(0)
-//////                .turn(Math.toRadians(90))
-//////                .lineToY(0)
-//////                .turn(Math.toRadians(90))
+//
+//                .splineTo(new Vector2d(51, -9), Math.toRadians(90))
+//                .lineToY(-52)
+//
+//                .splineTo(new Vector2d(61, -9), Math.toRadians(90))
+//
+//
+//                .lineToY(-52)
+
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/OfficalOpModes/Auton_BlueLeftHangPidf.java
@@ -0,0 +1,118 @@
+package org.firstinspires.ftc.teamcode.EPIC.OfficalOpModes;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.EPIC.Robot.Robot;
+import org.firstinspires.ftc.teamcode.EPIC.RobotStates.ArmStates;
+import org.firstinspires.ftc.teamcode.EPIC.RobotStates.ClawStates;
+import org.firstinspires.ftc.teamcode.EPIC.RobotStates.WristStates;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.SparkFunOTOSDrive;
+
+import com.acmerobotics.roadrunner.Pose2d;
+
+@Autonomous(name = "Auton_BlueLeftHangPidf")
+public class Auton_BlueLeftHangPidf extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Initialize the robot and mecanum drive
+        Robot odyssey = new Robot(this, "Blue");
+     //   MecanumDrive mecanumDrive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        SparkFunOTOSDrive drive= new SparkFunOTOSDrive(hardwareMap,new Pose2d(8.25, -63.85, Math.toRadians(90)));
+        Pose2d initialPos= new Pose2d(8.25, -63.85, Math.toRadians(90));
+        odyssey.setIsAutonomous(true);
+        odyssey.initialize();
+
+        while (opModeInInit()) {
+            idle();
+        }
+
+        TrajectoryActionBuilder tab = drive.actionBuilder(initialPos)
+                .lineToY(-32.15)
+
+                .strafeTo(new Vector2d(10, -37.4))
+
+                .splineToConstantHeading(new Vector2d(42, -9), Math.toRadians(90));
+        Action tsc1 = tab.build();
+//
+
+        waitForStart();
+        Actions.runBlocking(tsc1);
+        sleep(10000);
+
+        // Move forward to position (24, 0, 0)
+//        moveToPosition(drive, new Pose2d(24, 0, 0));
+
+        // Perform specimen pickup sequence
+//        performSpecimenPickup(odyssey);
+
+        // Perform specimen drop-off sequence
+//        performSpecimenDropoff(odyssey);
+
+        // Move backward to position (12, 0, 0)
+//        moveToPosition(drive, new Pose2d(12, 0, 0));
+    }
+
+    /**
+     * Moves the robot to a specific position using the MecanumDrive's trajectory capabilities.
+     */
+    private void moveToPosition(MecanumDrive mecanumDrive, Pose2d targetPose) {
+     /**   try {
+            MecanumDrive.FollowTrajectoryAction trajectoryAction = mecanumDrive.new FollowTrajectoryAction(
+                    [FollowTrajectoryAction]mecanumDrive.actionBuilder(mecanumDrive.pose)
+                            .lineToX(20)
+                            .build()
+            );
+
+            // Run the trajectory
+            while (opModeIsActive() && trajectoryAction.run()) {
+                // Wait for trajectory to complete
+            }
+        } catch (Exception e) {
+            // Handle trajectory execution errors
+        }*/
+    }
+
+    /**
+     * Handles the sequence for picking up a specimen.
+     */
+    private void performSpecimenPickup(Robot odyssey) throws InterruptedException {
+        odyssey.odysseyArm.move(ArmStates.SPECIMEN_PICK);
+        sleep(1000);
+
+        odyssey.odysseyClaw.move(ClawStates.OPEN);
+        sleep(1000);
+
+        odyssey.odysseyClaw.move(ClawStates.HOLDING_SAMPLE_PORTRAIT);
+        sleep(1000);
+    }
+
+    /**
+     * Handles the sequence for dropping off a specimen.
+     */
+    private void performSpecimenDropoff(Robot odyssey) throws InterruptedException {
+        odyssey.odysseyArm.move(ArmStates.SPECIMEN_DROP);
+        sleep(500);
+
+        odyssey.odysseyWrist.setPos(WristStates.PICKING_UP_SAMPLE);
+        sleep(500);
+
+        odyssey.odysseyWrist.setPos(WristStates.SPECIMEN_PICK);
+        sleep(500);
+
+        odyssey.odysseyClaw.move(ClawStates.OPEN);
+        sleep(500);
+
+        odyssey.odysseyWrist.setPos(WristStates.DEPOSITING_SAMPLE);
+        sleep(500);
+
+        odyssey.odysseyArm.move(ArmStates.INITIALIZED);
+        sleep(1000);
+    }
+}
