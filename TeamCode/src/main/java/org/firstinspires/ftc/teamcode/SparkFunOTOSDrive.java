2f5e2a9c944b64382a4252a7cd6fe7fcea9d21f8
oobty <zaidnajmi539@gmail.com>
2025-01-05 16:45:47 -0600

    Redid Pos offsets

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SparkFunOTOSDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SparkFunOTOSDrive.java
@@ -39,7 +39,7 @@ public class SparkFunOTOSDrive extends MecanumDrive {
         // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
 
         // RR localizer note: These units are inches and radians.
-        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(-11.9607, -2.6132, Math.toRadians(-0.0232));
+        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(-4.47, -0.985, Math.toRadians(0.416));
 
         // Here we can set the linear and angular scalars, which can compensate for
         // scaling issues with the sensor measurements. Note that as of firmware
