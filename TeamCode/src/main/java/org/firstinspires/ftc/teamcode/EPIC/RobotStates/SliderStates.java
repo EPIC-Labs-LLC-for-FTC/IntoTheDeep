1f0a5b146cdb0135bce25b194469e6bbcaa42eac
oobty <zaidnajmi539@gmail.com>
2025-01-23 18:43:58 -0600

    Updated state values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/RobotStates/SliderStates.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/RobotStates/SliderStates.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.EPIC.RobotStates;
 
 public enum                                                                                                                  SliderStates {
-    HIGH_BUCKET(3995),//3825 ORIGNAL
+    HIGH_BUCKET(3825),
     LOW_HANG(1),
     LOW_HANG_START(1750),
     RETRACTED(0.0);
