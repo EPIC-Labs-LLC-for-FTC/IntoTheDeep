a4bb9d07e87dd7d2fc2767d9dcbb176c00a755e6
oobty <zaidnajmi539@gmail.com>
2025-02-11 20:32:13 -0600

    Pedro Tuning 2/11

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/constants/LConstants.java
@@ -13,7 +13,7 @@ public class LConstants {
         OTOSConstants.hardwareMapName = "sensor_otos";
         OTOSConstants.linearUnit = DistanceUnit.INCH;
         OTOSConstants.angleUnit = AngleUnit.RADIANS;
-        OTOSConstants.offset = new SparkFunOTOS.Pose2D(-4.599, -1.244, Math.toRadians(-0.0003));
+        OTOSConstants.offset = new SparkFunOTOS.Pose2D(0, 0, Math.toRadians(0));
         OTOSConstants.linearScalar = 1.1587;
         OTOSConstants.angularScalar = 0.982;
     }
