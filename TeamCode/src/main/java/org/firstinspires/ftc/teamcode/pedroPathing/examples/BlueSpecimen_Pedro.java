b6edf5f39c5db9b5a7ef39dfc985e57624cff5f2
Nusaybah2011 <nusaybahniaziz@gmail.com>
2025-02-20 17:19:00 -0600

    Create BlueSpecimen_Pedro.java
    auton for specimen

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/examples/BlueSpecimen_Pedro.java
@@ -0,0 +1,306 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.examples;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.Path;
+import com.pedropathing.pathgen.PathChain;
+import com.pedropathing.pathgen.Point;
+import com.pedropathing.util.Constants;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.EPIC.Robot.Robot;
+import org.firstinspires.ftc.teamcode.EPIC.RobotStates.ArmStates;
+import org.firstinspires.ftc.teamcode.EPIC.RobotStates.ClawStates;
+import org.firstinspires.ftc.teamcode.EPIC.RobotStates.SliderStates;
+import org.firstinspires.ftc.teamcode.EPIC.RobotStates.WristStates;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
+import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
+
+@Autonomous(name = "BlueSpecimen_Pedro")
+public class BlueSpecimen_Pedro extends LinearOpMode {
+    public static double ap = 0.03, ai = 0, ad = 0.0015, af = 0.065;
+    public static double sp = 0.02, si = 0, sd = 0.001, sf = 0;
+
+    private Follower follower;
+    private Timer pathTimer, opmodeTimer;
+    private Robot odyssey;
+
+    private int pathState = 0;
+
+    private final Pose startPose = new Pose(8, 80, Math.toRadians(0));
+    private final Pose specimenDropPose = new Pose(28.5, 78);
+    private final Pose specimenBackPose = new Pose(28, 80);
+    private final Pose firstSamplePushPose = new Pose(25, 116);
+    private final Pose secondSamplePushPose = new Pose(25, 120, Math.toRadians(0));
+    private final Pose thirdSamplePushPose = new Pose(25, 124, Math.toRadians(0));
+    private final Pose sampleGrab = new Pose(25, 116);
+    private final Pose parkPose = new Pose(2, 15, Math.toRadians(270));
+
+    private Path goToPreload, moveToPark;
+    private PathChain scorePreload, pushFirstSample, pushSecondSample, pushThirdSample, grabSampleFromPlayer;
+
+    private void buildPaths() {
+
+        // Path for scoring preload
+        goToPreload = new Path(new BezierLine(new Point(startPose), new Point(specimenDropPose)));
+        goToPreload.setConstantHeadingInterpolation(0);
+
+
+        scorePreload = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(specimenDropPose), new Point(specimenBackPose)))
+                .setConstantHeadingInterpolation(0)
+                .build();
+        pushFirstSample = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(specimenBackPose), new Point(firstSamplePushPose)))
+                .setConstantHeadingInterpolation(0)
+                .build();
+
+        pushSecondSample = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(firstSamplePushPose), new Point(secondSamplePushPose)))
+                .setLinearHeadingInterpolation(firstSamplePushPose.getHeading(), secondSamplePushPose.getHeading())
+                .build();
+
+        pushThirdSample = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(secondSamplePushPose), new Point(thirdSamplePushPose)))
+                .setLinearHeadingInterpolation(secondSamplePushPose.getHeading(), thirdSamplePushPose.getHeading())
+//                .addParametricCallback(0.50, () -> performSlideDown(odyssey))
+                .build();
+
+        grabSampleFromPlayer = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(thirdSamplePushPose), new Point(sampleGrab)))
+                .setLinearHeadingInterpolation(thirdSamplePushPose.getHeading(), sampleGrab.getHeading())
+                .build();
+
+        scorePreload = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(sampleGrab), new Point(specimenDropPose)))
+                .setConstantHeadingInterpolation(0)
+                .build();
+
+
+        // Curved path for parking
+        //     park = new Path(new BezierCurve(new Point(scorePose), new Point(parkControlPose), new Point(parkPose)));
+        //    park.setLinearHeadingInterpolation(scorePose.getHeading(), parkPose.getHeading());
+    }
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
+
+        Constants.setConstants(FConstants.class, LConstants.class);
+        follower = new Follower(hardwareMap);
+        follower.setStartingPose(startPose);
+
+        odyssey = new Robot(this, "Blue", true);
+        odyssey.setIsAutonomous(true);
+        odyssey.initialize();
+
+        buildPaths();
+
+        Thread pidf = new Thread(() -> {
+            while (opModeIsActive()) {
+                odyssey.odysseySlider.runPIDF(sp, si, sd, sf);
+                odyssey.odysseyArm.runPIDF(ap, ai, ad, af);
+            }
+        });
+
+        while (opModeInInit()) {
+            telemetry.addData("Status", "Waiting for Start");
+            telemetry.update();
+        }
+
+        waitForStart();
+        pidf.start();
+        opmodeTimer.resetTimer();
+
+        while (opModeIsActive()) {
+            follower.update();
+            autonomousPathUpdate();
+
+            telemetry.addData("Path State", pathState);
+            telemetry.addData("X", follower.getPose().getX());
+            telemetry.addData("Y", follower.getPose().getY());
+            telemetry.addData("Heading", Math.toDegrees(follower.getPose().getHeading()));
+            telemetry.update();
+        }
+    }
+
+
+    private void autonomousPathUpdate() {
+        switch (pathState) {
+            case 0:
+                follower.setMaxPower(0.6);
+                follower.followPath(goToPreload);
+                pathState = 1;
+                break;
+
+            case 1:
+                follower.followPath(scorePreload);
+                pathState = 2;
+                break;
+
+            case 2:
+                if (!follower.isBusy()) {
+                    follower.followPath(pushFirstSample, true);
+                    pathState = 3;
+                }
+                break;
+
+            case 3:
+                if (!follower.isBusy()) {
+                    follower.followPath(pushSecondSample, true);
+                    pathState = 4;
+                }
+                break;
+
+            case 4:
+                if (!follower.isBusy()) {
+                    follower.followPath(pushThirdSample, true);
+                    pathState = 5;
+                }
+                break;
+
+            case 5:
+                if (!follower.isBusy()) {
+                    follower.followPath(grabSampleFromPlayer, true);
+                    // add method from grabbing sample from human player
+                    pathState = 6;
+                }
+                break;
+
+            case 6:
+                if (!follower.isBusy()) {
+                    follower.setMaxPower(0.6);
+                    follower.followPath(goToPreload);
+                    pathState = 7;
+                }
+                break;
+            case 7:
+                follower.followPath(scorePreload);
+                pathState = 8;
+                break;
+
+            case 8:
+                if (!follower.isBusy()) {
+                    performSpecimenDropoffUnder(odyssey);
+                    sleep(1000);
+                    follower.followPath(grabSampleFromPlayer, true);
+                    pathState = 9;
+                }
+                break;
+
+            case 9:
+                if (!follower.isBusy()) {
+                    follower.setMaxPower(0.6);
+                    follower.followPath(goToPreload);
+                    pathState = 10;
+                }
+                break;
+            case 10:
+                follower.followPath(scorePreload);
+                pathState = 11;
+                break;
+//
+//            case 8:
+//                if (!follower.isBusy()) {
+//                    pathState = -1;
+//                }
+//                break;
+
+
+        }
+    }
+
+    private void performSpecimenDropoff(Robot odyssey) {
+        odyssey.odysseyArm.move(ArmStates.SPECIMEN_DROP);
+        sleep(500);
+        odyssey.odysseyWrist.setPos(WristStates.PICKING_UP_SAMPLE);
+        sleep(500);
+        odyssey.odysseyClaw.move(ClawStates.OPEN);
+        sleep(1000);
+        //odyssey.odysseyClaw.move(ClawStates.HOLDING_SAMPLE_PORTRAIT);
+        // sleep(500);
+        odyssey.odysseyArm.move(ArmStates.AUTON_ARM_UP);
+        sleep(1000);
+    }
+
+    private void performSpecimenDropoffUnder(Robot odyssey) {
+        odyssey.odysseyArm.move(ArmStates.SPECIMEN_DROP);
+        sleep(500);
+
+        odyssey.odysseyWrist.setPos(WristStates.PICKING_UP_SAMPLE);
+        sleep(500);
+
+        odyssey.odysseyWrist.setPos(WristStates.INITIALIZING_AUTON);
+        sleep(500);
+
+        odyssey.odysseyArm.move(ArmStates.AUTON_BUCKET_DROP);
+        sleep(1600);
+
+        odyssey.odysseyClaw.move(ClawStates.OPEN);
+        sleep(500);
+    }
+
+    private void performSpecimenPickup(Robot odyssey) {
+        odyssey.odysseyWrist.setPos(WristStates.SPECIMEN_PICK);
+        odyssey.odysseyClaw.move(ClawStates.OPEN);
+        sleep(500);
+        odyssey.odysseyArm.move(ArmStates.SPECIMEN_PICK);
+        sleep(500);
+        odyssey.odysseyClaw.move(ClawStates.HOLDING_SAMPLE_PORTRAIT);
+        sleep(500);
+        odyssey.odysseyArm.move(ArmStates.READY_TO_DEPOSIT);
+    }
+
+    private void performSamplePickup(Robot odyssey) {
+
+        odyssey.odysseyClaw.move(ClawStates.OPEN);
+        sleep(500);
+
+        odyssey.odysseyArm.move(ArmStates.LOWERED);
+        sleep(500);
+
+        odyssey.odysseyWrist.setPos(WristStates.PICKING_UP_SAMPLE);
+        sleep(500);
+
+        odyssey.odysseyClaw.move(ClawStates.HOLDING_SAMPLE_PORTRAIT);
+        sleep(500);
+
+        odyssey.odysseyWrist.setPos(WristStates.INITIALIZING);
+        sleep(500);
+
+        odyssey.odysseyArm.move(ArmStates.AUTON_ARM_UP);
+        sleep(1000);
+
+        odyssey.odysseyClaw.move(ClawStates.OPEN);
+        sleep(500);
+
+        odyssey.odysseyArm.move(ArmStates.LOWERED);
+        sleep(1000);
+
+    }
+
+
+    private void performSlideUp(Robot odyssey) {
+        odyssey.odysseySlider.slide(SliderStates.HIGH_BUCKET);
+        sleep(2000);
+    }
+
+    private void performSlideDown(Robot odyssey) {
+        odyssey.odysseySlider.slide(SliderStates.RETRACTED);
+        sleep(1000);
+
+    }
+
+    private Runnable performSlideDownRunnable(Robot odyssey) {
+        odyssey.odysseySlider.slide(SliderStates.RETRACTED);
+        sleep(1000);
+        return null;
+    }
+}
+
