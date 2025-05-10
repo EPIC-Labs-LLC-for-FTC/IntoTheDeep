2458c04112d6531095fa7132bd9188a9ee061f3f
letsgo14 <teammba4@gmail.com>
2025-05-10 17:59:09 -0500

    Spec side 1 spec, push 3, park.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/examples/SampleFinalPedroNoSpec.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/examples/SampleFinalPedroNoSpec.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.pedroPathing.examples;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Pose;
-import com.pedropathing.pathgen.BezierCurve;
 import com.pedropathing.pathgen.BezierLine;
 import com.pedropathing.pathgen.Path;
 import com.pedropathing.pathgen.PathChain;
@@ -21,8 +20,8 @@ import org.firstinspires.ftc.teamcode.EPIC.RobotStates.WristStates;
 import org.firstinspires.ftc.teamcode.pedroPathing.constants.FConstants;
 import org.firstinspires.ftc.teamcode.pedroPathing.constants.LConstants;
 
-@Autonomous(name = "Spec_Final_Pedro")
-public class pedroSpec2 extends LinearOpMode {
+@Autonomous(name = "Sample_Final_Pedro_No_Spec")
+public class SampleFinalPedroNoSpec extends LinearOpMode {
     public static double ap = 0.03, ai = 0, ad = 0.0015, af = 0.065;
     public static double sp = 0.02, si = 0, sd = 0.001, sf = 0.1; //sf=0
 
@@ -38,22 +37,17 @@ public class pedroSpec2 extends LinearOpMode {
     private int nextPathState = 0;
 
     private final Pose startPose = new Pose(0, 0, Math.toRadians(0)); //x=0, y=0
-    private final Pose specimenDropPose = new Pose(29.25, 0);
-    private final Pose centerOfPushPose = new Pose(6,-8);
-    private final Pose pushPose = new Pose(50, -35, Math.toRadians(180));
-    private final Pose parkPose = new Pose(2, -35, Math.toRadians(180));
-    private final Pose centerOfPushPose2 = new Pose(27,-28);
-    private final Pose pushPose2 = new Pose(50, -45, Math.toRadians(180));
-    private final Pose parkPose2 = new Pose(3, -45, Math.toRadians(180));
-    private final Pose centerOfPushPose3 = new Pose(27,-40);
-    private final Pose pushPose3 = new Pose(50, -51.5, Math.toRadians(180));
-    private final Pose parkPose3 = new Pose(4, -51.5, Math.toRadians(180));
-    private final Pose pickupPose = new Pose(21, 41);
-    private final Pose specimenDropPose2 = new Pose(29.25, -1.5);
+    private final Pose specimenDropPose = new Pose(29.25, -1.5);
     private final Pose specimenBackPose = new Pose(21, -1);
+    private final Pose firstPickupPose = new Pose(18, 41);
+    private final Pose secondPickupPose = new Pose(18.5, 51);//21.5, 51
+    private final Pose thirdPickupPose = new Pose(12, 52, Math.toRadians(0));
+    private final Pose depositPose = new Pose(0, 52, Math.toRadians(315));
+    private final Pose bucketBackPose = new Pose(8, 49, Math.toRadians(0));
+    private final Pose parkPose = new Pose(2, 15, Math.toRadians(270));
 
     private Path goToPreload, moveToPark;
-    private PathChain scorePreload, grabPickup1, scoreSpec, pushSample, park, pushSample2, park2, pushSample3, park3;
+    private PathChain scorePreload, grabPickup1, backFromBucket, grabPickup2, grabPickup3, scorePickup1, scorePickup2, scorePickup3;
 
     private void buildPaths() {
 
@@ -61,39 +55,46 @@ public class pedroSpec2 extends LinearOpMode {
         goToPreload = new Path(new BezierLine(new Point(startPose), new Point(specimenDropPose)));
         goToPreload.setConstantHeadingInterpolation(0);
 
+        // Path chains for picking up and scoring samples
+
         scorePreload = follower.pathBuilder()
                 .addPath(new BezierLine(new Point(specimenDropPose), new Point(specimenBackPose)))
                 .setConstantHeadingInterpolation(0)
                 .build();
-        pushSample = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(specimenDropPose), new Point(centerOfPushPose), new Point(pushPose)))
-                .setLinearHeadingInterpolation(centerOfPushPose.getHeading(), pushPose.getHeading())
+        grabPickup1 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(specimenDropPose), new Point(firstPickupPose))) // first one is sepecimenBackPose
+                .setConstantHeadingInterpolation(0)
                 .build();
-        park = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(pushPose), new Point(parkPose)))
-                .setLinearHeadingInterpolation(pushPose.getHeading(), parkPose.getHeading())
+        scorePickup1 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(firstPickupPose), new Point(depositPose)))
+                .setLinearHeadingInterpolation(firstPickupPose.getHeading(), depositPose.getHeading())
                 .build();
-        pushSample2 = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(parkPose), new Point(centerOfPushPose2), new Point(pushPose2)))
-                .setLinearHeadingInterpolation(parkPose.getHeading(), pushPose2.getHeading())
-                .build();
-        park2 = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(pushPose2), new Point(parkPose2)))
-                .setLinearHeadingInterpolation(pushPose2.getHeading(), parkPose2.getHeading())
-                .build();
-        pushSample3 = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(parkPose2), new Point(centerOfPushPose3), new Point(pushPose3)))
-                .setLinearHeadingInterpolation(parkPose2.getHeading(), pushPose3.getHeading())
-                .build();
-        park3 = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(pushPose3), new Point(parkPose3)))
-                .setLinearHeadingInterpolation(pushPose3.getHeading(), parkPose3.getHeading())
-                .build();
-        scoreSpec = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(pickupPose), new Point(specimenDropPose2)))
-                .setLinearHeadingInterpolation(pickupPose.getHeading(), specimenDropPose2.getHeading())
+        backFromBucket = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(depositPose), new Point(bucketBackPose)))
+                .setLinearHeadingInterpolation(depositPose.getHeading(), bucketBackPose.getHeading())
                 .build();
 
+        grabPickup2 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(bucketBackPose), new Point(secondPickupPose)))
+                .setLinearHeadingInterpolation(bucketBackPose.getHeading(), secondPickupPose.getHeading())
+                //.addParametricCallback(0.60, () -> performSlideDown(odyssey))
+                .build();
+
+        scorePickup2 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(secondPickupPose), new Point(depositPose)))
+                .setLinearHeadingInterpolation(secondPickupPose.getHeading(), depositPose.getHeading())
+                .build();
+
+        grabPickup3 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(depositPose), new Point(thirdPickupPose)))
+                .setLinearHeadingInterpolation(depositPose.getHeading(), thirdPickupPose.getHeading())
+                .addParametricCallback(0.30, () -> performSlideDown(odyssey))
+                .build();
+
+        scorePickup3 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(thirdPickupPose), new Point(depositPose)))
+                .setLinearHeadingInterpolation(thirdPickupPose.getHeading(), depositPose.getHeading())
+                .build();
 
 
     }
@@ -150,62 +151,78 @@ public class pedroSpec2 extends LinearOpMode {
     private void autonomousPathUpdate() {
         switch (pathState) {
             case 0: // strafe to submirsable, working
-                follower.setMaxPower(1);
+                follower.setMaxPower(1); // 0.6 (UPDATED)
                 follower.followPath(goToPreload);
                 follower.update();
-                nextPathState = 1;
+                nextPathState = 1; //1
                 pathState = 100;
                 break;
 
-            case 1: // score spec
+            case 1: // move back from submirsable, working
                 performSpecimenDropoffUnder(odyssey);
+                //follower.followPath(scorePreload);
+                //follower.update();
                 nextPathState = 2;
                 pathState = 2; //2
                 break;
 
-            case 2: // goes to sample and pushes
-                follower.followPath(pushSample);
+            case 2: // move from submirsable back pose to first sample, working
+                follower.followPath(grabPickup1);
                 follower.update();
-                nextPathState = 3; //3
+                nextPathState = 3;//3
                 pathState = 100;
                 break;
 
-            case 3: //push sample to observation zone
-                follower.followPath(park);
+            case 3: //picking up sample, working + score sample
+                performSamplePickup1(odyssey);
+                sleep(500);
+                performSlideUp(odyssey);
+                follower.setMaxPower(0.6);
+                follower.followPath(scorePickup1, true);
                 follower.update();
+                maxFollowerBusyTime = 1.75;
                 nextPathState = 4; //4
                 pathState = 100;
                 break;
 
             case 4: // back up from bucket
-                    follower.followPath(pushSample2);
+                    maxFollowerBusyTime = 3; //3
+                    follower.followPath(backFromBucket, true);
                     follower.update();
                     nextPathState = 5; //5
                     pathState = 100;
                 break;
 
             case 5: // slides down + sample pick up or arm up
-                follower.followPath(park2);
-                follower.update();
-                nextPathState = 6; //5
-                pathState = 100;
+            performSlideDown(odyssey);
+                    if(pathTimer.getElapsedTimeSeconds()<26.0){ // time should be 15.0 for 1+1
+                        follower.followPath(grabPickup2, true);
+                        follower.update();
+                        nextPathState = 6;
+                        pathState = 100;
+                    }
+                    else{
+                        sleep(1200);
+                        odyssey.odysseyArm.move(ArmStates.AUTON_ARM_UP);
+                        pathState = 7;
+                    }
+
                 break;
 
-            case 6: // back up from bucket
-                follower.followPath(pushSample3);
+            case 6: // pick up + score sample 2
+                performSamplePickup2(odyssey);
+                sleep(500);
+                performSlideUp(odyssey);
+                sleep(300);
+                follower.followPath(scorePickup2, true);
                 follower.update();
-                nextPathState = 7; //5
+                maxFollowerBusyTime = 1.75;
+                nextPathState = 4;
                 pathState = 100;
+
                 break;
 
-            case 7: // slides down + sample pick up or arm up
-                follower.followPath(park3);
-                follower.update();
-                nextPathState = 8; //5
-                pathState = 100;
-                break;
-
-            case 8: // end state
+            case 7: // end state
                 //if(!follower.isBusy()) {
                     telemetry.addData("path state", pathState);
                     telemetry.addData("x", follower.getPose().getX());
@@ -235,12 +252,12 @@ public class pedroSpec2 extends LinearOpMode {
                     followerBusyInitalAccess = 0;
                 }
                 else if ( timeInFollowerBusyState > maxFollowerBusyTime) {
-                    /* if (nextPathState == 4){
+                    if (nextPathState == 4){
                         pathState = 4;
                     }
                     else {
-                        pathState = 8;//nextPathState; //temporary, change to backup, slides down, arm up
-                    } */
+                        pathState = 7;//nextPathState; //temporary, change to backup, slides down, arm up
+                    }
                     followerBusyInitalAccess = 0;
                 }
                 else {
