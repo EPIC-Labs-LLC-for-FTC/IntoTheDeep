afa51f469dd50841547180d969fff875931b9756
Ashfaq Chougle <ashfaqchougle@hotmail.com>
2025-10-08 22:24:46 -0500

    Update Test_Motors.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/TestOpModes/Test_Motors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EPIC/TestOpModes/Test_Motors.java
@@ -20,47 +20,51 @@ public class Test_Motors extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         nlm1 = hardwareMap.get(DcMotorEx.class, "nlm1");
         int sleepval = 1000;
-        double reset = 0;
-        nlm1.setPower(reset);
+//        double reset = 0;
+//        nlm1.setPower(reset);
         nlm1.setDirection(DcMotorSimple.Direction.FORWARD);
-        nlm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        nlm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        nlm1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        nlm1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         nlm1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        int armPos = nlm1.getCurrentPosition();
-        int targetPos = armPos;
+//        int armPos = nlm1.getCurrentPosition();
+//        int targetPos = armPos;
         waitForStart();
         while (opModeIsActive()) {
 
             if(gamepad1.b) {
-                armPos = nlm1.getCurrentPosition();
-                targetPos = armPos+1000;
-                //sleep(500);
+//                armPos = nlm1.getCurrentPosition();
+//                targetPos = armPos+1000;
+//                //sleep(500);
             }
             else if(gamepad1.x) {
-                armPos = nlm1.getCurrentPosition();
-                targetPos = armPos-1000;
-
-                telemetry.addData("targetPos", targetPos);
-                telemetry.update();
-                //sleep(100);
+//                armPos = nlm1.getCurrentPosition();
+//                targetPos = armPos-1000;
+//
+//                telemetry.addData("targetPos", targetPos);
+//                telemetry.update();
+//                //sleep(100);
             }
             else if(gamepad1.a) {
-                targetPos=0;
-                //sleep(100);
+//                targetPos=0;
+//                //sleep(100);
             }
             else if(gamepad1.y) {
+                nlm1.setPower(1);
 
                 //sleep(100);
             }
-            if(targetPos>=0){
-                nlm1.setTargetPosition(targetPos);
-                nlm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                nlm1.setPower(1);
-                sleep(1000);
+            else if(gamepad1.dpad_down){
+
             }
+//            if(targetPos>=0){
+//                nlm1.setTargetPosition(targetPos);
+//                nlm1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//                nlm1.setPower(1);
+//                sleep(1000);
+//            }
             nlm1.setPower(0);
-            telemetry.addData("nlm1", nlm1.getCurrentPosition());
-            telemetry.update();
+//            telemetry.addData("nlm1", nlm1.getCurrentPosition());
+//            telemetry.update();
         }
     }
 }
